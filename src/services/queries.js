0fac95321a25609d13d07c7652a602dba0c12f8d
CaseyCameron <mcasey.cameron@gmail.com>
2022-01-04 11:47:04 -0800

    Complete

--- src/services/queries.js
+++ src/services/queries.js
@@ -1,32 +1,45 @@
 import { checkError, client } from './client.js';
 export async function getMovies() {
-  // return the list of all movies
+  const req = await client.from('movies').select('*');
+  return checkError(req);
 }
 
 export async function getMoviesWithDirector() {
-  // return the list of all the movies with their director
+  const req = await client.from('movies').select(`
+      *, 
+      directors(
+        id
+      )
+    `);
+  return checkError(req);
 }
 
 export async function getDirectorNames() {
-  // return the list of the director's names
+  const req = await client.from('directors').select('name');
+  return checkError(req);
 }
 
 export async function getMovieById(id) {
-  // return the movie with the given id
+  const req = await client.from('movies').select('*').match({ id }).single();
+  return checkError(req);
 }
 
 export async function getMovieByTitle(title) {
-  // return the movie with the given title
+  const req = await client.from('movies').select('*').match({ title }).single();
+  return checkError(req);
 }
 
 export async function getOldestMovie() {
-  // return the oldest movie (assume the database is not sorted)
+  const req = await client.from('movies').select('*').order('year', { ascending: true }).limit(1).single();
+  return checkError(req);
 }
 
 export async function getMoviesAfter(year) {
-  // return movies made after the year passed in
+  const req = await client.from('movies').select('*').gte('year', year);
+  return checkError(req);
 }
 
 export async function getHighestGrossingMovie() {
-  // return movie with the highest box office total
+  const req = await client.from('movies').select('*').order('box_office', { ascending: false }).limit(1).single();
+  return checkError(req);
 }
